d93de836ec66b7ba2945462de6ec24fd56713c83
FolkNatchaon <146178428+FolkNatchaon@users.noreply.github.com>
2025-08-30 00:59:56 +0700

    MainPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 const HomePage = () => {
   return (
-    <div className="flex h-screen justify-between">
-      <div className="flex justify-center items-center w-full h-full bg-paper">
-        <div className="">
-          <p className="text-[40px] font-bold">Hi, I am</p>
-          <p className="text-[70px] font-bold">Natchanon Srijudanu</p>
-          <p className="text-[32px] font-extrabold text-gray-600">Bachelor of Information Technology and Innovation</p>
+    <div className="flex h-screen justify-between bg-[#FBF8CC]">
+      <div className="flex justify-center items-center w-full h-full">
+        <div className="text-[#03045E]">
+          <p className="text-[32px]">Hello, I am</p>
+          <p className="text-[100px] font-extrabold">Natchanon</p>
+          <p className="text-[26px]">Bachelor of Information Technology and Innovation</p>
         </div>
       </div>
 
-      <div className="flex justify-center items-center w-full h-full bg-papaya p-10">
+      <div className="flex justify-center items-center w-full h-full">
         <p className="text-[100px]">picture</p>
       </div>
     </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,21 @@
-import { getDefaultAutoSelectFamily } from "net"
+import Link from "next/link"
 
 const NavBar = () => {
     return (
-        <nav className="flex p-8 w-full fixed justify-between items-center">
+        <nav className="flex p-8 w-full fixed justify-between items-center text-[#03045E]">
             {/* Left */}
-            <div className="ml-10 text-gray-900 text-[20px] font-extrabold">
-            <h1>HOME</h1>
+            <div className="ml-10 text-[32px] font-extrabold">
+                <Link href={"/"}><h1 className="hover:underline cursor-pointer">HOME</h1></Link>
             </div>
 
             {/* Right */}
-            <div className="flex items-center gap-[30px] text-white text-[20px] font-extrabold">
-                <p >ABOUT ME</p>
+            <div className="flex items-center gap-[30px] text-[20px]">
+                <Link href={"/about"}><p className="hover:underline cursor-pointer">ABOUT ME</p></Link>
 
-                <p>PROJECT</p>
+                <Link href={"/skill"}><p className="hover:underline cursor-pointer">SKILL</p></Link>
 
-                <p>CONTACT</p>
+                <Link href={"/project"}><p className="hover:underline cursor-pointer">PROJECT</p></Link>
+
             </div>
         </nav>
     )
