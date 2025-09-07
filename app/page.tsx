de0fff81880d4b37a0974a85f499c202de86526f
FolkNatchaon <146178428+FolkNatchaon@users.noreply.github.com>
2025-09-08 00:31:22 +0700

    NewPortfolioAgain

--- app/page.tsx
+++ app/page.tsx
@@ -1,135 +1,159 @@
+import DynamicText from "@/components/animation/dynamic-text"
+import { FaLaptopCode } from "react-icons/fa"
+import { FaDatabase } from "react-icons/fa"
+import { FaTools } from "react-icons/fa"
+import Image from 'next/image';
+
 const HomePage = () => {
 	return (
-		<div id="home" className="bg-blue-dark flex w-screen flex-col text-white">
-			{/* Title */}
-			<div className="item-center flex h-screen w-screen justify-center">
-				<div className="item-center mt-90 flex justify-center">
-					<div className="px-4 text-center">
-						<div className="">
-							<h1 className="mb-6 text-4xl md:text-6xl">
-								Hello, I am{" "}
-								<h1 className="text-blue-400"> Natchanon Srijudanu</h1>
-							</h1>
-						</div>
-						<div>
-							<p className="text-[20px]">
-								Passionate about building innovative web applications with
-								modern technologies.
-								<br />
-								Currently seeking opportunities to grow and contribute to
-								meaningful projects.
-							</p>
-						</div>
-					</div>
-				</div>
+		<section className="flex w-full flex-col">
+			{/* Home */}
+			<div
+				id="home"
+				className="flex h-screen flex-col items-center justify-center"
+			>
+				<h1 className="text-[40px] leading-none font-bold">
+					NATCHANON SRIJUDANU
+				</h1>
+
+				<DynamicText texts={["SOFTWARE DEVELOPER", "UX/UI DESIGNER"]} />
 			</div>
-			{/* AboutMe */}
+			{/* About Me */}
 			<div
 				id="aboutme"
-				className="bg-blue-fade flex h-screen flex-col justify-center p-20"
+				className="flex h-screen items-center justify-center bg-gray-200"
 			>
-				<div className="flex flex-col items-center justify-center">
-					<div>
-						<h1 className="text-[40px] font-extrabold">About Me</h1>
-					</div>
-					<div className="mt-20 flex items-center justify-between text-[24px]">
-						<div className="w-1/2">Picture</div>
-						<div className="flex w-1/2 flex-col gap-2">
-							<p>
-								Hey there! I'm a studying in Bangkok University with a passion
-								for web development. I interest in frontend and backend,
-							</p>
-							<p>
-								enjoying the process of building functional and scalable web
-								applications.
-							</p>
-							<p>With expertise in modern JavaScript frameworks</p>
+				<div className="items-center justify-center">
+					<div className="flex items-center justify-between gap-2">
+						{/* Left */}
+						<div className="flex w-1/2 flex-col gap-6">
+							<div>
+								<h1 className="text-[32px] font-semibold">My Story</h1>
+								<div className="text-[20px]">
+									<p>
+										Hello, my name is Folk. I am currently studying Computer
+										Science and have a strong interest in developing Full-Stack
+										Software Applications.
+									</p>
+									<p>
+										I have a foundation in HTML, CSS, JavaScript, as well as
+										frameworks such as Tailwind CSS and Next.js,and
+									</p>
+									<p>
+										I am working to further develop my skills toward building
+										fully functional systems.
+									</p>
+								</div>
+							</div>
+							<hr className="border-t border-black"></hr>
+							<div>
+								<h2 className="text-[32px] font-semibold">Education</h2>
+								<p className="text-[16px]">
+									School of Information Technology and Innovation | Bangkok
+									University
+								</p>
+								<p className="text-[16px]">2022 - Present</p>
+							</div>
+						</div>
+						{/* Right */}
+						<div className="justify-center items-center">
+							<Image className="rounded-full"
+								src="/IMG_6452.jpg"
+								width={300}
+								height={300}
+								alt="Profile"
+							/>
 						</div>
 					</div>
 				</div>
 			</div>
-			{/* Skill and Technology */}
-			<div id="skill" className="flex w-screen p-20">
-				<div className="w-full">
-					{/* Skills Title*/}
-					<div className="mt-15 flex flex-col items-center">
-						<div className="flex items-center gap-4 py-5">
-							<div className="w-[300px] border-b-[2px] text-white"></div>
-							<span className="mx-4 flex-shrink text-[50px] font-extrabold text-white">
-								Skill and Technology
-							</span>
-							<div className="w-[300px] border-b-[2px] text-white"></div>
-						</div>
+			{/* Skill */}
+			<div id="skill" className="flex flex-col p-10">
+				<div className="mt-30 flex flex-col justify-center gap-24">
+					{/* Title */}
+					<div className="flex flex-col items-center justify-center">
+						<h1 className="text-[40px] font-extrabold">Skills & Tools</h1>
 					</div>
-					{/* Frontend Dev */}
-					<div className="mt-40 flex w-full flex-col items-center">
-						<div>
-							<p className="text-[38px] font-extrabold">Frontend Development</p>
-						</div>
-						<div className="mt-10 flex items-center gap-20 text-[20px] font-bold">
-							<div className="rounded-[10] border-2 p-3">
-								<p>Python</p>
+					{/* Header */}
+					<div className="flex flex-col items-center justify-center gap-18">
+						{/* Frontend */}
+						<div className="flex flex-col items-center justify-center">
+							<div className="flex items-center gap-2">
+								<div className="flex h-12 w-12 items-center justify-center rounded-[10px] bg-gray-200">
+									<FaLaptopCode className="text-[24px] text-black" />
+								</div>
+								<h2 className="text-[28px] font-semibold">
+									Frontend Development
+								</h2>
 							</div>
-							<div className="rounded-[10] border-2 p-3">
-								<p>Java</p>
-							</div>
-							<div className="rounded-[10] border-2 p-3">
-								<p>JavaScript</p>
-							</div>
-							<div className="rounded-[10] border-2 p-3">
-								<p>TypeScript</p>
-							</div>
-							<div className="rounded-[10] border-2 p-3">
-								<p>C++</p>
-							</div>
-						</div>
-					</div>
-					{/* Framework */}
-					<div className="mt-25 flex w-full flex-col items-center">
-						<div>
-							<p className="text-[38px] font-extrabold">Backend Developer</p>
-						</div>
-						<div className="mt-10 flex items-center gap-20 text-[20px] font-bold">
-							<div className="rounded-[10] border-2 p-3">
-								<p>Tailwind CSS</p>
-							</div>
-							<div className="rounded-[10] border-2 p-3">
-								<p>Next.js</p>
-							</div>
-							<div className="rounded-[10] border-2 p-3">
-								<p>React</p>
-							</div>
-							<div className="rounded-[10] border-2 p-3">
-								<p>Node.js</p>
+							<div className="mt-6 flex items-center justify-center gap-5 text-[16px]">
+								<div className="flex h-32 w-32 flex-col items-center justify-center rounded-[10px] border-1 p-4">
+									JavaScript
+								</div>
+								<div className="flex h-32 w-32 flex-col items-center justify-center rounded-[10px] border-1 p-4">
+									TypeScript
+								</div>
+								<div className="flex h-32 w-32 flex-col items-center justify-center rounded-[10px] border-1 p-4">
+									Tailwind CSS
+								</div>
+								<div className="flex h-32 w-32 flex-col items-center justify-center rounded-[10px] border-1 p-4">
+									Next.js
+								</div>
 							</div>
 						</div>
-					</div>
-					{/*Tools*/}
-					<div className="mt-25 flex w-full flex-col items-center">
-						<div>
-							<p className="text-[38px] font-extrabold">Tools</p>
+						{/* Backend */}
+						<div className="flex flex-col items-center justify-center">
+							<div className="flex items-center gap-2">
+								<div className="flex h-12 w-12 items-center justify-center rounded-[10px] bg-gray-200">
+									<FaDatabase className="text-[24px] text-black" />
+								</div>
+								<h2 className="text-[28px] font-semibold">
+									Backtend Development
+								</h2>
+							</div>
+							<div className="mt-6 flex items-center justify-center gap-5 text-[16px]">
+								<div className="flex h-32 w-32 flex-col items-center justify-center rounded-[10px] border-1 p-4">
+									Python
+								</div>
+								<div className="flex h-32 w-32 flex-col items-center justify-center rounded-[10px] border-1 p-4">
+									C#
+								</div>
+								<div className="flex h-32 w-32 flex-col items-center justify-center rounded-[10px] border-1 p-4">
+									Node.js
+								</div>
+								<div className="flex h-32 w-32 flex-col items-center justify-center rounded-[10px] border-1 p-4">
+									MySQL
+								</div>
+							</div>
 						</div>
-						<div className="mt-10 flex items-center gap-20 text-[20px] font-bold">
-							<div className="rounded-[10] border-2 p-3">
-								<p>Git/Github</p>
+						{/* Tool */}
+						<div className="flex flex-col items-center justify-center">
+							<div className="flex items-center gap-2">
+								<div className="flex h-12 w-12 items-center justify-center rounded-[10px] bg-gray-200">
+									<FaTools className="text-[24px] text-black" />
+								</div>
+								<h2 className="text-[28px] font-semibold">
+									Tools & Technologies
+								</h2>
 							</div>
-							<div className="rounded-[10] border-2 p-3">
-								<p>Figma</p>
-							</div>
-							<div className="rounded-[10] border-2 p-3">
-								<p>MySQL</p>
-							</div>
-							<div className="rounded-[10] border-2 p-3">
-								<p>PowerBI</p>
-							</div>
-							<div className="rounded-[10] border-2 p-3">
-								<p>RapidMiner</p>
+							<div className="mt-6 flex items-center justify-center gap-5 text-[16px]">
+								<div className="flex h-32 w-32 flex-col items-center justify-center rounded-[10px] border-1 p-4">
+									Figma
+								</div>
+								<div className="flex h-32 w-32 flex-col items-center justify-center rounded-[10px] border-1 p-4">
+									Git/Github
+								</div>
+								<div className="flex h-32 w-32 flex-col items-center justify-center rounded-[10px] border-1 p-4">
+									Vercel
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+			{/* Project */}
+			<div className="h-screen bg-gray-200"></div>
+		</section>
 	)
 }
 
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,7 @@ import DynamicText from "@/components/animation/dynamic-text"
 
 const HomePage = () => {
 	return (
-		<section className="flex w-full flex-col">
+		<section id="home" className="flex w-full flex-col">
 			{/* Title */}
 			<div className="flex h-screen flex-col items-center justify-center">
 				<h1 className="text-[40px] leading-none font-bold">
@@ -12,6 +12,8 @@ const HomePage = () => {
 
 				<DynamicText texts={["SOFTWARE DEVELOPER", "UX/UI DESIGNER"]} />
 			</div>
+
+			<div></div>
 		</section>
 	)
 }
